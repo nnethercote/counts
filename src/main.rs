a64301ee85b8832bdd6743374efb6efa3fd82013
Nicholas Nethercote <n.nethercote@gmail.com>
2023-07-28 16:39:19 +1000

    Release 1.0.3.

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ OPTIONS:
     -e             Erase weights after applying, replacing them with `NNN`
 ";
 
-const VERSION: &str = "1.0.2";
+const VERSION: &str = "1.0.3";
 
 fn do_main() -> io::Result<()> {
     // Process args.
